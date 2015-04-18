ba91e5fc78f5c6d54bd5ed3fd7f5944fbd881f2d
Jared Prather <jared.prather@centare.com>
2015-04-18 17:56:35 -0500

    Added placeholder assets, sprite updates

---
+++ Leafy The Cabbage/Assets/Scripts/Character/CharacterLifeSpriteController.cs
@@ -0,0 +1,25 @@
+﻿using LeafyTheCabbage.Domain.Character;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Character
+{
+    public class CharacterLifeSpriteController : MonoBehaviour
+    {
+        public Dictionary<CharacterLifeState, Sprite> SpriteLibrary = new Dictionary<CharacterLifeState,Sprite>();
+
+        private CharacterLifeState currentState;
+
+        public void SetState(CharacterLifeState state)
+        {
+            if (SpriteLibrary.ContainsKey(state))
+            {
+                currentState = state;
+                this.GetComponent<SpriteRenderer>().sprite = SpriteLibrary[state];
+            }
+        }
+    }
+}
---
+++ Leafy The Cabbage/Assets/Scripts/Character/MovementSpriteController.cs
@@ -0,0 +1,51 @@
+﻿using LeafyTheCabbage.Domain.Character;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Character
+{
+    public class MovementSpriteController : MonoBehaviour
+    {
+        public Dictionary<CharacterMovementState, Sprite> SpriteLibrary = new Dictionary<CharacterMovementState,Sprite>();
+
+        private CharacterMovementState currentState;
+
+        // Use this for initialization
+        void Start()
+        {
+            foreach (CharacterMovementState state in Enum.GetValues(typeof(CharacterMovementState)))
+            {
+                try
+                {
+                    
+                    var sprite = Resources.Load<Sprite>(state.ToString() + ".png"); 
+                    SpriteLibrary.Add(state, sprite);
+                    Debug.Log("Loaded sprite for state: " + state.ToString());
+                }
+                catch
+                {
+
+                }
+                
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void SetState(CharacterMovementState state)
+        {
+            if (SpriteLibrary.ContainsKey(state))
+            {
+                currentState = state;
+                this.GetComponent<SpriteRenderer>().sprite = SpriteLibrary[state];
+            }
+        }
+    }
+}
