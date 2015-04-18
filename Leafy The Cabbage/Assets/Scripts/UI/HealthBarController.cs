d52f8d4db4d19e33f4fb7a1f43c31f7d0923f6dc
Kelly O'Callaghan <kelly.ocallaghan@centare.com>
2015-04-18 18:34:37 -0500

    Health bar first passsss

---
+++ Leafy The Cabbage/Assets/Scripts/UI/HealthBarController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using LeafyTheCabbage.Domain.Character;
+using System.Collections;
+
+public class HealthBarController : MonoBehaviour
+{
+    public Text HealthBar;
+
+	// Use this for initialization
+	void Start () {
+        HealthBar = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        HealthBar.text = "Health: " + this.GetComponent<HealthComponent>().CurrentHealth;
+	}
+}
