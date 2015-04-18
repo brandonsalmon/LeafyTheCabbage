aa416529eef573cb91862cdd233370b701a2bfe2
Jared Prather <jared.prather@centare.com>
2015-04-18 12:27:45 -0500

    Health and death

---
+++ Leafy The Cabbage/Assets/Scripts/Character/HealthComponent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthComponent : MonoBehaviour {
+
+    public int CurrentHealth;
+    public int MaxHealth;
+    public int MinimumHealth;
+
+    public bool IsDead
+    {
+        get
+        {
+            return CurrentHealth <= MinimumHealth;
+        }
+    }
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CurrentHealth--;
+	}
+}
