0885a3621f517f25ffa48a211bce2178acb513e9
Kelly O'Callaghan <kelly.ocallaghan@centare.com>
2015-04-19 18:49:52 -0500

    Venis slowly drops lazers

--- Leafy The Cabbage/Assets/Scripts/ShootScript.cs
+++ Leafy The Cabbage/Assets/Scripts/ShootScript.cs
@@ -9,16 +9,23 @@ public class ShootScript : MonoBehaviour
 
     public bool BulletsEatHealth = true;
 
+    public bool UseAI = false;
+
+    private float _nextShootTime;
+
     // Use this for initialization
     void Start()
     {
-
+        if (UseAI)
+        {
+            _nextShootTime = UnityEngine.Random.Range(1, 3);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Slash) )//|| CrossPlatformInputManager.GetButton("Shoot"))
+        if (!UseAI && Input.GetKeyDown(KeyCode.Slash) )//|| CrossPlatformInputManager.GetButton("Shoot"))
         {
             // Create a new bullet at “transform.position”
             // Which is the current position of the game object
@@ -28,6 +35,16 @@ public class ShootScript : MonoBehaviour
             {
                 var health = gameObject.GetComponent<HealthComponent>();
                 health.UpdateHealth(-5);
+            }
+        }
+
+        if (UseAI)
+        {
+            _nextShootTime -= Time.deltaTime;
+            if (_nextShootTime < 0)
+            {
+                Instantiate(bullet, transform.position, Quaternion.identity);
+                _nextShootTime = UnityEngine.Random.Range(1, 3);
             }
         }
     }
