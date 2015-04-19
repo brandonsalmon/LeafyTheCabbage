6a61e026e0849f287fa3e0069ffac76feae6e742
Daniel Laughland <daniel.laughland@centare.com>
2015-04-18 20:02:47 -0500

    Noises!

---
+++ Leafy The Cabbage/Assets/Scripts/Character/CharacterAudioController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class CharacterAudioController : MonoBehaviour {
+
+    public AudioClip wallBumpMed;
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        if (coll.collider.tag == "CollidableWall") {
+			GetComponent<AudioSource>().PlayOneShot(wallBumpMed, 0.7f);
+        }
+    }
+}
