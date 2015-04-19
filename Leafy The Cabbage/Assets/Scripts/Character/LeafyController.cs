cddd40e8e0f3a11b08d743b921303a46ecbb8195
Jared Prather <jared.prather@centare.com>
2015-04-18 20:21:56 -0500

    Leafy now changes sprites based on state

--- Leafy The Cabbage/Assets/Scripts/Character/LeafyController.cs
+++ Leafy The Cabbage/Assets/Scripts/Character/LeafyController.cs
@@ -11,23 +11,34 @@ namespace Assets.Scripts.Character
     {
         public CharacterLifeState LifeState = CharacterLifeState.Alive;
         public CharacterMovementState MovementState = CharacterMovementState.Idle;
+        private HealthComponent healthComp;
+        private MoveController moveController;
 
         // Use this for initialization
         void Start()
         {
-
+            this.healthComp = this.gameObject.GetComponent<HealthComponent>();
+            this.moveController = this.gameObject.GetComponent<MoveController>();
         }
 
         // Update is called once per frame
         void Update()
         {
-            var healthComp = this.GetComponent<HealthComponent>();
+            //if (healthComp == null)
+            //{
+            //    healthComp = this.gameObject.GetComponent<HealthComponent>();
+            //}
+            //if (moveController == null)
+            //{
+            //    moveController = this.gameObject.GetComponent<MoveController>();
+            //}
             // Check if the character is dead
             if (healthComp.IsDead)
             {
                 this.LifeState = CharacterLifeState.Dead;
             }
-            MovementState = CharacterMovementState.Jumping;
+            MovementState = moveController.MovementState;
+            Debug.Log("Current moveController state: " + moveController.MovementState.ToString());
             // Set appropriate sprites
             //this.GetComponent<CharacterLifeSpriteController>().SetState(LifeState);
             this.GetComponent<MovementSpriteController>().SetState(MovementState);
