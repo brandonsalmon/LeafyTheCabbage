219e18a8f302f231db75cbd81a0b29626ffc7ee1
Kelly O'Callaghan <kelly.ocallaghan@centare.com>
2015-04-19 14:00:14 -0500

    LEAFY CAN SHOOT! ENEMIES CAN TAKE BULLET DAMAGE! REJOICE!!!  Add ShootScript to your leafy. Add EnemyTakeDamage & HealthComponent to your enemy. Configure damage/health amounts. Barrels of fun.

---
+++ Leafy The Cabbage/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+	// Public variable 
+    public int Force = 50;
+
+
+    // Gets called once when the bullet is created
+    void Start () {
+        var rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.AddForce(Vector2.right * Force, ForceMode2D.Impulse);
+    }
+
+    // Gets called when the object goes out of the screen
+    void OnBecameInvisible() {  
+        // Destroy the bullet 
+        Destroy(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
---
+++ Leafy The Cabbage/Assets/Scripts/ShootScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootScript : MonoBehaviour
+{
+
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Slash))
+        {
+            // Create a new bullet at “transform.position”
+            // Which is the current position of the game object
+            Instantiate(bullet, transform.position, Quaternion.identity);
+        }
+	}
+}
