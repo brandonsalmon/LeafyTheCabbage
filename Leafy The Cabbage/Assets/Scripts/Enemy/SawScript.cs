1814d54bdb2cddbd1798d7fe5e5a88642f9395ab
Kelly O'Callaghan <kelly.ocallaghan@centare.com>
2015-04-19 16:21:52 -0500

    Made fancy shredder that changes its sprite when you hit it

--- Leafy The Cabbage/Assets/Scripts/Enemy/SawScript.cs
+++ Leafy The Cabbage/Assets/Scripts/Enemy/SawScript.cs
@@ -2,6 +2,12 @@
 
 public class SawScript : MonoBehaviour
 {
+    public Sprite WornSprite;
+
+    public Sprite MegaKilledSprite;
+
+    private bool hasFirstKill;
+
     // Use this for initialization
     void Start()
     {
@@ -18,7 +24,18 @@ public class SawScript : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            if (hasFirstKill)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = MegaKilledSprite;
+            }
+            else
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = WornSprite;
+            }
+
             other.gameObject.GetComponent<HealthComponent>().Kill();
+
+            hasFirstKill = true;
         }
         else
         {
