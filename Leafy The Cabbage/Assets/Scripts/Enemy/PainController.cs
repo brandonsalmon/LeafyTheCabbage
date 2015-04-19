1119ef0e91fab68de33872a7197844f7d3949677
Kelly O'Callaghan <kelly.ocallaghan@centare.com>
2015-04-18 19:15:34 -0500

    Added PainController - drop on anything with a collider and set the pain amount.

---
+++ Leafy The Cabbage/Assets/Scripts/Enemy/PainController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PainController : MonoBehaviour
+{
+
+    public int Damage = -10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        
+        if (other.collider.tag == "Player")
+        {
+            other.collider.GetComponent<HealthComponent>().UpdateHealth(Damage);
+        }
+    }
+}
