ec639f127fc7c4a37dc374027b9cf59de209bd75
Brandon Salmon <brandon.j.salmon@gmail.com>
2015-04-18 19:00:31 -0500

    Saw of death

---
+++ Leafy The Cabbage/Assets/SawScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SawScript : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.back * Time.deltaTime * 360);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            var level = gameObject.GetComponentInParent<LevelController>();
+            level.ReloadPlayerAtCheckpoint(other.gameObject);
+        }
+        else
+        {
+            Destroy(other);
+        }
+    }
+}
