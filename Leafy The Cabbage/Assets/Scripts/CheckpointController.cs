e438afad3942407dc2587af8ebc4fe3e5b600df0
Daniel Laughland <daniel.laughland@centare.com>
2015-04-19 19:34:27 -0500

    Checkpoint sound effect

--- Leafy The Cabbage/Assets/Scripts/CheckpointController.cs
+++ Leafy The Cabbage/Assets/Scripts/CheckpointController.cs
@@ -2,10 +2,20 @@
 
 public class CheckpointController : MonoBehaviour {
 
+	public AudioClip checkpointReachedSound;
+
+	private bool hasBeenHit = false;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+			if (!hasBeenHit)
+			{
+				var audio = other.GetComponent<AudioSource>();
+				audio.PlayOneShot(checkpointReachedSound);
+				hasBeenHit = true;
+			}
             var level = gameObject.GetComponentInParent<LevelController>();
             level.SetLastCheckpoint(transform.position);
         }
